94494c8280f3edb19a3f5f0f3bc9ced3974661d6
Bruno Fernandes <brunocpf@outlook.com>
2024-09-24 23:52:35 -0300

    add theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,10 @@ const config: Config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        sans: ["var(--josefin-sans)", "ui-sans-serif", "system-ui"],
+        serif: ["var(--josefin-slab)", "ui-serif", "Georgia"],
+      },
     },
   },
   plugins: [tailwindcssAnimate],
